1b89c973454dc6d1075b26537f76d63cc53fd907
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-17 16:36:44 -0400

    add env configs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,13 @@
 export const signing_secret = process.env.signing_secret;
 export const token = process.env.token;
-export const name = "Hello World!"
+export const journal_channel = process.env.journal_channel;
+export const name = "Paevik"
+
+export const apiKey = process.env.FirebaseapiKey
+export const authDomain = process.env.FirebaseauthDomain
+export const databaseURL = process.env.FirebasedatabaseURL
+export const projectId = process.env.FirebaseprojectId
+export const storageBucket = process.env.FirebasestorageBucket
+export const messagingSenderId = process.env.FirebasemessagingSenderId
+export const appId = process.env.FirebaseappId
+export const measurementId = process.env.FirebasemeasurementId
