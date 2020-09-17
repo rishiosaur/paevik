0ba926ae4e62b469fc7c17fd4c50598c99a1fe4f
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-17 16:36:35 -0400

    add basic exports

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,5 @@
 // This is where you should export all the features. Every folder in the starter kit follows this spec.
+export { default as journal } from "./journal";
 
-export { default as feature1 } from './feature1';
+export { default as onboarding } from "./onboarding";
+export { default as messaging } from "./message";
