6e2c6d7d6ffc77f3b76d5ee502e2634136652f00
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-18 06:58:56 -0400

    create homeview

---
+++ src/features/home/blocks/homeViewBlocks.ts
@@ -0,0 +1,56 @@
+import { AppHomeOpenedEvent } from "@slack/bolt";
+
+export function homeViewBlocks(event: AppHomeOpenedEvent) {
+  return [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": "The journal!",
+        "emoji": true
+      }
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `Welcome, <@${event.user}>! :sparkles: :book: :pen:—This is where you'll find all of your journal-related things. DM <@UHFEGV147> if you need some help.\n`,
+      },
+    },
+    {
+      "type": "context",
+      "elements": [
+        {
+          "type": "mrkdwn",
+          "text": ":pencil: Manually start an entry with `/entry `\n :scrappy: Add a journal entry to Scrapbook with `/scrapbook <id>`"
+        }
+      ]
+    },
+    {
+      "type": "divider"
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*Basic Actions*`,
+      },
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: ":pencil: Start a journal entry",
+          },
+
+          value: event.user,
+          action_id: "startEntry"
+        },
+      ],
+    },
+  ];
+}
