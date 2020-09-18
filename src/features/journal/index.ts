fd603b47b8854259e36ba3245ffac78babe153f3
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-18 09:20:00 -0400

    Add message to database

--- src/features/journal/index.ts
+++ src/features/journal/index.ts
@@ -5,19 +5,23 @@ import { updatePostEntryMessage } from './functions/updatePostEntryMessage';
 import { db } from '../../index';
 const journal = async (app: App) => {
   app.action("postEntry", async ({ ack, context, body, action }) => {
-    const x = await postEntry(ack, action, body, `<@${body.user.id}>`);
-    console.log(x)
+    const postEntryDone = await postEntry(ack, action, body, `<@${body.user.id}>`);
     await updatePostEntryMessage((body as any).message.ts, (body as any).container.channel_id, true)
 
-    // await db.collection("users").doc(body.user.id).collection("entries").doc((action as any).value).update({
-    //   message: x.message.ts
-    // })
+    await db.collection("users").doc(body.user.id).collection("entries").doc((action as any).value).update({
+      message: (postEntryDone as any).message.ts
+    })
+
   });
 
   app.action("postEntryAnonymously", async ({ ack, context, body, action }) => {
-    await postEntry(ack, action, body, `*Anonymous*`);
+    const postEntryDone = await postEntry(ack, action, body, `*Anonymous*`);
 
     await updatePostEntryMessage((body as any).message.ts, (body as any).container.channel_id, false)
+
+    await db.collection("users").doc(body.user.id).collection("entries").doc((action as any).value).update({
+      message: (postEntryDone as any).message.ts
+    })
   });
 
   app.action("noPost", async ({ ack, context, body, action }) => {
