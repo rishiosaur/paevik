7c8c18c9648b628ffcf439a928542c8bfdbaf0e0
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-18 06:59:00 -0400

    export entries

---
+++ src/features/entries/index.ts
@@ -0,0 +1,38 @@
+import { App } from "@slack/bolt";
+import { stat } from "fs";
+import {
+  filterDM,
+  filterChannelType,
+  filterNoBotMessages,
+} from "../../middleware/index";
+import { getUserState, setUserState } from "../../shared/db/index";
+import { createEntry } from "./functions/createEntry";
+import { postMessageCurry } from "../../shared/messages/index";
+import { createEntryBlocks } from "./blocks/createEntryBlocks";
+
+const messaging = async (app: App) => {
+  app.message(
+    filterChannelType("im"),
+    filterNoBotMessages,
+    async ({ message, body, say }) => {
+      const { user, text } = message;
+      const state = await getUserState(user);
+
+      const im = postMessageCurry(user);
+
+      if (state === "creatingEntry") {
+        const { id } = await createEntry(user, text);
+
+        await im(createEntryBlocks(user, id));
+        await setUserState(user, "submitted");
+      } else {
+        say(
+          "I'm not sure I understand. If you want to create an entry, run `/entry`!"
+        );
+      }
+    }
+  );
+};
+
+export default messaging;
+
