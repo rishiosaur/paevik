22dbd2e220d7abfddb9bcb79453ec511e489f1c7
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-18 06:59:19 -0400

    create entry blocks func

---
+++ src/features/entries/blocks/createEntryBlocks.ts
@@ -0,0 +1,41 @@
+import { journal_channel } from "../../../config";
+import { getTime } from "../../../shared/time/index";
+
+export function createEntryBlocks(user: string, id: string): any[] {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `That's an awesome entry, <@${user}>! Thanks for writing. Do you want to publicly share it to the <#${journal_channel}> channel for others to comment on? I've written it to my database as part of *${getTime()}*'s entries.`,
+      },
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: "Yep!",
+          },
+          style: "primary",
+          value: id,
+          action_id: "postEntry",
+        },
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: "No thanks.",
+          },
+          style: "danger",
+          value: id,
+          action_id: "noPost",
+        },
+      ],
+    },
+  ];
+}
