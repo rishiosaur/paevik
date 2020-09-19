a725332e16fbf5d48ee37300f94511a941103604
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-19 19:18:06 -0400

    add postmessage, format

--- src/features/onboarding/functions/onboarding.ts
+++ src/features/onboarding/functions/onboarding.ts
@@ -1,9 +1,13 @@
 import { AckFn } from '@slack/bolt'
-import { postMessageCurry } from '../../../shared/messages/index'
+import {
+	postMessageCurry,
+	postEphemeralDMCurry,
+} from '../../../shared/messages/index'
 import {
 	checkIfUserExists,
 	createUser,
 	setUserState,
+	getUserState,
 } from '../../../shared/db/index'
 import { welcomeBlocks } from '../blocks/welcomeBlocks'
 import { returningBlocks } from '../blocks/returningBlocks'
@@ -12,6 +16,7 @@ export const onboard = async (ack: AckFn<any>, user_id: string) => {
 	await ack()
 
 	const im = postMessageCurry(user_id)
+	const ephemeralDm = postEphemeralDMCurry(user_id)
 	const exists = await checkIfUserExists(user_id)
 
 	if (!exists) {
@@ -21,6 +26,11 @@ export const onboard = async (ack: AckFn<any>, user_id: string) => {
 		)
 
 		await createUser(user_id)
+	} else if ((await getUserState(user_id)) == 'creatingEntry') {
+		await ephemeralDm(
+			undefined,
+			"Looks like you're still in the process of writing another entry. Let's finish that one before making a new one, shall we? ;P"
+		)
 	} else {
 		im(returningBlocks(user_id), 'Welcome back to Paevik!')
 		setUserState(user_id, 'creatingEntry')
