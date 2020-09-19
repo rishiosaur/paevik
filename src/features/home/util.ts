60e0c6860fde4aa11cb24274c677284344af9eb3
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-19 14:09:47 -0400

    Delete util

--- src/features/home/util.ts
+++
@@ -1,9 +0,0 @@
-import { AppHomeOpenedEvent } from "@slack/bolt";
-import { homeViewBlocks } from "./blocks/homeViewBlocks";
-
-export function baseHomeView(event: AppHomeOpenedEvent) {
-  return {
-    type: "home",
-    blocks: homeViewBlocks(event),
-  };
-}
