15d8860e125c48aa75df97f2f73d267082e822b7
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-19 14:07:13 -0400

    Slash commands

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,5 @@
-export { default as journal } from "./journal";
+export { default as journal } from "./journal-channel";
 export { default as onboarding } from "./onboarding";
 export { default as entries } from "./entries";
 export { default as home } from "./home";
+export { default as commands } from './commands'
