f5554061017c5d1423b88703a81d2213b0d604b3
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-19 14:07:57 -0400

    Hoist up entry

--- src/features/entries/functions/createEntry.ts
+++ src/features/entries/functions/createEntry.ts
@@ -3,14 +3,10 @@ import { Entry } from '../../../types/index';
 import { getTime } from '../../../shared/time/index';
 
 
-export function createEntry(user: string, entry: string) {
+export function createEntry(user: string, entry: Entry) {
   return db
     .collection("users")
     .doc(user)
     .collection("entries")
-    .add({
-      date: getTime(),
-      entry: entry,
-      submitted: false,
-    } as Entry);
+    .add(entry);
 }
