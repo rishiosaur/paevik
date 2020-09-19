12d2dc6e33b7983308f51da9a63fbfe1d400416d
Luke Carapezza <me@lukec.me>
2020-09-19 23:59:32 +0000

    prettier fix
    looks like codespaces don't come with prettier installed

--- src/shared/time/index.ts
+++ src/shared/time/index.ts
@@ -6,4 +6,5 @@ const slackFormat = '{date_short_pretty} at {time}'
 const isoTime = () => moment.format(momentFormat)
 const currentUnix = () => moment.unix()
 
-export const currentDate = () => `<!date^${currentUnix}^${slackFormat}|${isoTime}>`
+export const currentDate = () =>
+    `<!date^${currentUnix()}^${slackFormat}|${isoTime()}>`
