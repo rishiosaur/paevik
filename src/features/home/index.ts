b236163555a60471e33e11b0cd7f091ce2f5b5d3
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-19 21:08:29 -0400

    generate homeview types

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,13 +1,15 @@
 import { App } from '@slack/bolt'
 import { generateBaseHomeView } from './functions/generateBaseHomeView'
+import { getLastEntries } from '../entries/functions/getLastEntries'
 const home = async (app: App) => {
 	app.event('app_home_opened', async ({ event, client }) => {
 		try {
 			// Call views.publish with the built-in client
+			console.log(await getLastEntries(event.user))
 			const result = await client.views.publish({
 				// Use the user ID associated with the event
 				user_id: event.user,
-				view: generateBaseHomeView(event) as any,
+				view: (await generateBaseHomeView(event)) as any,
 			})
 
 			console.log(result)
