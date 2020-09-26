38d12790df667439dc972ffaa726e5a0acab5b7c
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-26 08:27:13 -0400

    add file code

--- src/features/journal-channel/functions/postToJournal.ts
+++ src/features/journal-channel/functions/postToJournal.ts
@@ -1,26 +1,37 @@
+import { app, db } from '../../../index'
+import { File, Entry } from '../../../types/index'
 import { currentDate } from '../../../shared/time/index'
 import { postMessage } from '../../../shared/messages'
-import { db } from '../../../index'
-import { journal_channel } from '../../../config'
+
+import { journal_channel, user_token } from '../../../config'
+
+const getLinkFromFile = async (file: File) =>
+	`${file.url_private}?pub_secret=${[
+		...((
+			await app.client.files.sharedPublicURL({
+				file: file.id,
+				token: user_token,
+			})
+		).file as File).permalink_public.split('-'),
+	].pop()}`
 
 export const postToJournal = async (
 	user: string,
 	value: string,
 	id: string,
-	userId: string
+	userId: string,
+	entry: Entry
 ) => {
 	const ref = db.collection('users').doc(userId).collection('entries').doc(id)
 
 	const emoji = ['parrot_love', 'bellhop_bell', 'pencil', 'thinkspin']
 	const random = emoji[Math.floor(Math.random() * emoji.length)]
 
-	// console.log((["hello"] as Array<any>))
-
 	await ref.update({
 		submitted: true,
 	})
 
-	return postMessage(journal_channel, [
+	const initialBlocks = [
 		{
 			type: 'section',
 			text: {
@@ -35,5 +46,17 @@ export const postToJournal = async (
 				text: `> ${value}`,
 			},
 		},
-	] as Array<any>)
+	] as Array<any>
+
+	const fileBlocks: Array<any> = await entry.files?.map(async (file) => ({
+		type: 'image',
+		image_url: await getLinkFromFile(file),
+		alt_text: file.name,
+		title: file.title,
+	}))
+
+	return postMessage(
+		journal_channel,
+		await [...initialBlocks, ...(await Promise.all(fileBlocks))]
+	)
 }
